b52458b37df590283e69b2fb5d3d12c42a57a726
Matthew Chan <matt@themattchan.com>
2014-05-23 22:33:03 -0700

    window object

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Snake
 =====
 
-Learning Scala's object system and how to do GUI programming in Scala by remaking classic games.
+Learning Scala's object system and how to do GUI programming in Scala.
--- Snake.scala
+++ Snake.scala
@@ -4,3 +4,13 @@
  */
 
 package snake
+
+import scala.swing._
+import scala.swing.event._
+
+object SnakeApp extends SimpleGUIApplication {
+  def top = new MainFrame {
+	title = "Snake"
+	contents = ???
+  }
+}
