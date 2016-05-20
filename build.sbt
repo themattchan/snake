8206662775ca6a2616f612d37d11cc9103e3408f
Matthew Chan <matt@themattchan.com>
2016-05-19 22:35:21 -0700

    how does this work

--- build.sbt
+++ build.sbt
@@ -3,5 +3,6 @@ name := "Snake"
 version := "1.0"
 
 libraryDependencies += "org.scala-lang.modules" % "scala-swing_2.11" % "1.0.1"
+libraryDependencies += "io.reactivex" %% "rxscala" % "0.26.1"
 
 mainClass in (Compile,run) := Some("snake.SnakeGui")
--- src/main/scala/Snake.scala
+++ src/main/scala/Snake.scala
@@ -1,30 +1,48 @@
 package snake
 
-import swing._
-import swing.event._
+import scala.util.Random
+
+import scala.swing._
+import scala.swing.Reactions.Reaction
+import scala.swing.event.Event
+// TODO: try RXScala
+// import rx.lang.scala.Observable
+// import rx.lang.scala.Observer
+// import rx.lang.scala.Subscription
+// import rx.lang.scala.subscriptions.Subscription
 
 object SnakeGui extends SimpleSwingApplication {
 
-  val gui = new BorderPanel {
-    minimumSize = new Dimension(500, 500)
+  override def startup(args: Array[String]) {
+    val game =
+      if (args.length == 2) new Game(args(0).toInt, args(1).toInt)
+      else new Game(50, 50) // default
 
-    listenTo(keys)
-
-    contents += new TextField
-
-    reactions += {
-      // keyPressed(_,key,_,_) match {
-      //   case Up    => ???
-      //   case Down  => ???
-      //   case Left  => ???
-      //   case Right => ???
-      // }
+    val t = new MainFrame {
+      title = "Snake"
+      contents = new BorderPanel {
+        layout(game) = Center
+      }
     }
+
+    if (t.size == new Dimension(0,0)) t.pack()
+    t.visible = true
   }
+}
+
+trait Space
+case class Wall() extends Space
+case class Body() extends Space
+case object Empty extends Space
+
+class Game(width: Int, height: Int) extends GridPanel(width,height) {
+  type Board = Array[Array[Space]]
+
+  val cellSize = 10 // px on each side of cell
+  val drawSize = new Dimension(width * cellSize, height * cellSize)
+
+  val board = Array.fill(height)(Array.fill(width)(Empty)) //Array.ofDim[Space](width,height)
 
 
-  def top = new MainFrame {
-    title = "Snake"
-    contents = gui
-  }
+  var snake = new List()
 }
